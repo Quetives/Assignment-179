c89623b469b7c49e99b19057ee6449b3d4e682ce
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 12:41:49 -0700

    Add project files.

---
+++ Assignment 179/Employee.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_176
+{
+    public class Employee : Person, IQuittable
+    {
+        public override string SayName()
+        {
+            Console.WriteLine("Employee");
+            return base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("I Quit!");
+        }
+    }
+}
---
+++ Assignment 179/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_176
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employeeOne = new Employee() { firstName = "Sample", lastName = "Student" };
+            //employeeOne.SayName();
+
+            IQuittable employeeTwo = new Employee();
+            employeeTwo.Quit();
+            Console.ReadLine();
+        }
+
+    }
+}
